92dab8930432dd06f90b3bbe650e6ddd6f5716a0
noah <114136737+YnoahY@users.noreply.github.com>
2022-11-11 16:16:18 +0900

    Delete CustomerAccess.kt

--- CustomerAccess.kt
+++
@@ -1,8 +0,0 @@
-package classAndConstructor
-
-class CustomerAccess {
-    companion object {
-        const val LEVEL = "INTERMEDIATE"
-        @JvmStatic fun longin() = println("Login...")//어노테이션 표기 사용
-    }
-}
