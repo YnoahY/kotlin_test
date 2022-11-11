e6b396280e01150ec87379d9ed541a3bd2f0ae5e
noah <114136737+YnoahY@users.noreply.github.com>
2022-11-11 16:16:07 +0900

    Delete Customer.java

--- Customer.java
+++
@@ -1,9 +0,0 @@
-package classAndConstructor;
-
-//자바의 customer클래스
-public class Customer {
-    public static final String LEVEL = "BASIC"; //static 필드
-    public static void longin() { //static 메서드
-         System.out.println("Login...");
-    }
-}
